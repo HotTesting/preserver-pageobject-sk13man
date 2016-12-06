485362a51e546965a5e63d4b6364c2856efac750
Sergey Golovin <s.golovin@trip-top.com>
2016-12-07 01:26:58 +0200

    Changed IDE to WebStorm and Pluged Reporter

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,14 +2,27 @@ module.exports.config = {
     useAllAngular2AppRoots: true,
     specs: '*_spec.js',
     directConnect: true,
-    
+    jasmineNodeOpts: {
+        print: function() {}
+    },
+
+   
 onPrepare: function() { 
+     let  JasmineConsoleReporter = require('jasmine-console-reporter')
+jasmine.getEnv().addReporter(new JasmineConsoleReporter({
+    colors: 1,           // (0|false)|(1|true)|2
+    cleanStack: 1,       // (0|false)|(1|true)|2|3
+    verbosity: 4,        // (0|false)|1|2|(3|true)|4
+    listStyle: 'indent', // "flat"|"indent"
+    activity: true
+}),
+// pass the initialized reporter to whichever task or host...
   
    beforeEach(function () {
       
       browser.get('http://www.hiteshbalar.com/preserver/notes')
-      browser.sleep(3000)
-
-    })
-        }
+      browser.sleep(2000)
+  })
+        
+)}
 }
