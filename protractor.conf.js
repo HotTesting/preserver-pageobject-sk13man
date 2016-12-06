96ddc589a303e186a00c2b107c1ec383511d97b5
Sergey Golovin <s.golovin@trip-top.com>
2016-12-06 17:42:30 +0200

    After multiply attempts, I still can not to plug in Reporter(

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,20 +1,15 @@
 module.exports.config = {
     useAllAngular2AppRoots: true,
-    specs: 'spec.js',
+    specs: '*_spec.js',
     directConnect: true,
-   
-jasmineNodeOpts: {
-  showColours: true
-},
-
-
+    
 onPrepare: function() { 
   
    beforeEach(function () {
+      
       browser.get('http://www.hiteshbalar.com/preserver/notes')
       browser.sleep(3000)
 
     })
-
-}
+        }
 }
